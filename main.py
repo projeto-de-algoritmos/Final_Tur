5624440f2c11c63eef895f2131eea497aa785713
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-12-09 04:44:28 -0300

    Adiciona mais elementos no template

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def pagina_principal():
-    numeroNos = 4
+    numeroNos = 8
 
     matrizDistancias = [[0] * numeroNos for i in range(numeroNos)]
 
